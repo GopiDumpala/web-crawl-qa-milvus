490f7f3853d5eb74724468ec17a9af42a602b40e
GopiDumpala <136985029+GopiDumpala@users.noreply.github.com>
2024-07-15 11:22:50 +0530

    Update cuda_crawler.py

--- cuda_crawler.py
+++ cuda_crawler.py
@@ -9,7 +9,7 @@ class CudaDocSpider(scrapy.Spider):
         content = response.css('body').getall()
         yield {'url': response.url, 'content': content}
 
-        if response.meta.get('depth', 0) < 1:
+        if response.meta.get('depth', 0) < 5:
             next_links = response.css('a::attr(href)').getall()
             for link in next_links:
                 yield response.follow(link, self.parse, meta={'depth': response.meta.get('depth', 0) + 1})
@@ -17,7 +17,7 @@ class CudaDocSpider(scrapy.Spider):
 process = CrawlerProcess(settings={
     'FEED_FORMAT': 'json',
     'FEED_URI': 'cuda_docs.json',
-    'DEPTH_LIMIT': 1
+    'DEPTH_LIMIT': 5
 })
 process.crawl(CudaDocSpider)
 process.start()
